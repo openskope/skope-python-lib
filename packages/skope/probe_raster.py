1f4d14e9357dfde9e5323e0cc42d9ac2344e3627
Timothy McPhillips <tmcphillips@absoluteflow.org>
2017-01-09 22:33:48 -0800

    Refactored formatting of raster file properties.

--- packages/skope/probe_raster.py
+++ packages/skope/probe_raster.py
@@ -7,19 +7,43 @@ from osgeo import gdal
 from skope.raster_file import RasterFile
 
 def print_properties(dataset):
-    print('Format              {}'.format(dataset.format))
-    print('Origin (deg)        {} lng, {} lat'.format(dataset.origin_lng, dataset.origin_lat))
-    print('Pixel size (deg)    {:f} (1/{:d}) lng x {:f} (1/{:d}) lat'
-        .format(dataset.pixel_size_lng, dataset.pixels_per_degree_lng, 
-                dataset.pixel_size_lat, dataset.pixels_per_degree_lat))
-    print('Raster extent (deg) {:f} lng x {:f} lat'
-        .format(dataset.extent_in_degrees_lng, dataset.extent_in_degrees_lat))
-    print('Raster extent (px)  {} x {} '
-        .format(dataset.extent_in_pixels_lng, dataset.extent_in_pixels_lat))
-    print('Number of bands     {}'.format(dataset.band_count))
-    print('Block size          {} x {}'.format(dataset.block_size_x, dataset.block_size_y))
-    print('No-data value       {}'.format(dataset.no_data_value))
-    print('Pixel data type     {}'.format(dataset.data_type_name))
+    
+    print_row('Format', 
+        dataset.format )
+    
+    print_row('Origin (deg)', '{}, {}'.format(
+        dataset.origin_lng, 
+        dataset.origin_lat))
+    
+    print_row('Pixel size (deg)', '{:f} x {:f} (1/{:d} x 1/{:d})'.format(
+        dataset.pixel_size_lng,
+        dataset.pixel_size_lat, 
+        dataset.pixels_per_degree_lng, 
+        dataset.pixels_per_degree_lat))
+    
+    print_row('Raster extent (deg)', '{:f} x {:f}'.format(
+        dataset.extent_in_degrees_lng,
+        dataset.extent_in_degrees_lat))
+    
+    print_row('Raster extent (px)', '{} x {}'.format(
+        dataset.extent_in_pixels_lng, 
+        dataset.extent_in_pixels_lat))
+    
+    print_row('Number of bands',
+        dataset.band_count)
+    
+    print_row('Block size', '{} x {}'.format(
+        dataset.block_size_x, 
+        dataset.block_size_y))
+    
+    print_row('No-data value', 
+        dataset.no_data_value)
+
+    print_row('Pixel data type', 
+        dataset.data_type_name)
+
+def print_row(label, value):
+    print('{} | {}'.format(label.ljust(19), value))
 
 if __name__ == '__main__':
     
