a8ae70b7ccc79bfe97b0910cbaae8f8c700501b1
Timothy McPhillips <tmcphillips@absoluteflow.org>
2017-01-09 16:31:11 -0800

    Finished first iteration of RasterFile class.

--- packages/skope/raster_file.py
+++ packages/skope/raster_file.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
-import optparse
+import argparse
 import sys
 
 from enum import Enum
 from osgeo import gdal
+import osr
 from gdalconst import GA_ReadOnly
 
 class RasterDataType(Enum): (UNKNOWN, BYTE, UINT16, INT16, 
@@ -23,65 +24,56 @@ class RasterFile:
                 raise Exception ("Could not open raster file: " + filename)
             
         # store given dataset properties
-        self.properties = { 
-            'filename': self.dataset.GetDescription(),
-            'metadata': self.dataset.GetMetadata_Dict(),
-            'format': self.dataset.GetDriver().LongName,
-            'projection': self.dataset.GetProjection(),
-            'geotransform': self.dataset.GetGeoTransform(),
-            'extent_in_pixels_lng': self.dataset.RasterXSize,
-            'extent_in_pixels_lat': self.dataset.RasterYSize,
-            'band_count': self.dataset.RasterCount
-        }
+        self.filename = self.dataset.GetDescription()
+
+        self.metadata = self.dataset.GetMetadata_Dict()
+        self.format = self.dataset.GetDriver().LongName
+        self.projection = self.dataset.GetProjection()
+        self.geotransform = self.dataset.GetGeoTransform()
+        self.extent_in_pixels_lng = self.dataset.RasterXSize
+        self.extent_in_pixels_lat = self.dataset.RasterYSize
+        self.band_count = self.dataset.RasterCount
 
         # derive additional dataset properties
-        self.properties['origin_lng'] = self.properties['geotransform'][0]
-        self.properties['origin_lat'] = self.properties['geotransform'][3]
-        self.properties['pixel_size_lng'] = abs(self.properties['geotransform'][1])
-        self.properties['pixel_size_lat'] = abs(self.properties['geotransform'][5])
-        self.properties['pixels_per_degree_lng'] = int(1.0 / self.properties['pixel_size_lng'])
-        self.properties['pixels_per_degree_lat'] = int(1.0 / self.properties['pixel_size_lat'])
-        self.properties['extent_in_degrees_lng'] = self.properties['extent_in_pixels_lng'] * self.properties['pixel_size_lng']
-        self.properties['extent_in_degrees_lat'] = self.properties['extent_in_pixels_lat'] * self.properties['pixel_size_lat']
+        self.origin_lng = self.geotransform[0]
+        self.origin_lat = self.geotransform[3]
+        self.pixel_size_lng = abs(self.geotransform[1])
+        self.pixel_size_lat = abs(self.geotransform[5])
+        self.pixels_per_degree_lng = int(1.0/self.pixel_size_lng)
+        self.pixels_per_degree_lat = int(1.0/self.pixel_size_lat)
+        self.extent_in_degrees_lng = self.extent_in_pixels_lng * self.pixel_size_lng
+        self.extent_in_degrees_lat = self.extent_in_pixels_lat * self.pixel_size_lat
 
-        # extract self.properties of band 1 
+        # extract properties of band 1 
         band = self.dataset.GetRasterBand(1)
-        self.properties['block_size_x'], self.properties['block_size_y']= band.GetBlockSize()
-        self.properties['no_data_value'] = band.GetNoDataValue()
-        self.properties['data_type'] = RasterDataType(band.DataType)
-        self.properties['data_type_name'] = RasterDataType(band.DataType).name
+        self.block_size_x, self.block_size_y = band.GetBlockSize()
+        self.no_data_value = band.GetNoDataValue()
+        self.data_type = RasterDataType(band.DataType)
+        self.data_type_name = RasterDataType(band.DataType).name
 
         # confirm that all bands have identical self.properties
-        for band_index in range(1, self.properties['band_count']):
+        for band_index in range(1, self.band_count):
             band = self.dataset.GetRasterBand(band_index)
-            if (band.GetBlockSize()[0] != self.properties['block_size_x'] or
-                band.GetBlockSize()[1] != self.properties['block_size_y'] or
-                band.GetNoDataValue() != self.properties['no_data_value'] or
-                RasterDataType(band.DataType) != self.properties['data_type']):
-                println("Bands 1 and " + band_index + " have different self.properties.")
-
+            if (band.GetBlockSize()[0] != self.block_size_x or
+                band.GetBlockSize()[1] != self.block_size_y or
+                band.GetNoDataValue() != self.no_data_value or
+                RasterDataType(band.DataType) != self.data_type):
+                    println("Bands 1 and " + band_index + " have different properties.")
 
     def print_properties(self):
-
-        print('Format              {}'
-            .format(self.properties['format']))
-        print('Origin (deg)        {} lng, {} lat'
-            .format(self.properties['origin_lng'], self.properties['origin_lat']))
+        print('Format              {}'.format(self.format))
+        print('Origin (deg)        {} lng, {} lat'.format(self.origin_lng, self.origin_lat))
         print('Pixel size (deg)    {:f} (1/{:d}) lng x {:f} (1/{:d}) lat'
-            .format(self.properties['pixel_size_lng'],  self.properties['pixels_per_degree_lng'], 
-                    self.properties['pixel_size_lat'], self.properties['pixels_per_degree_lat']))
+            .format(self.pixel_size_lng, self.pixels_per_degree_lng, 
+                    self.pixel_size_lat, self.pixels_per_degree_lat))
         print('Raster extent (deg) {:f} lng x {:f} lat'
-            .format(self.properties['extent_in_degrees_lng'], self.properties['extent_in_degrees_lat']))
+            .format(self.extent_in_degrees_lng, self.extent_in_degrees_lat))
         print('Raster extent (px)  {} x {} '
-            .format(self.properties['extent_in_pixels_lng'], self.properties['extent_in_pixels_lat']))
-        print('Number of bands     {}'
-            .format(self.properties['band_count']))
-        print('Block size          {} x {}'
-            .format(self.properties['block_size_x'], self.properties['block_size_y']))
-        print('No-data value       {}'
-            .format(self.properties['no_data_value']))
-        print('Pixel data type     {}'
-            .format(self.properties['data_type_name']))
+            .format(self.extent_in_pixels_lng, self.extent_in_pixels_lat))
+        print('Number of bands     {}'.format(self.band_count))
+        print('Block size          {} x {}'.format(self.block_size_x, self.block_size_y))
+        print('No-data value       {}'.format(self.no_data_value))
+        print('Pixel data type     {}'.format(self.data_type_name))
 
     #    min = band.GetMinimum()
     #    max = band.GetMaximum()
@@ -97,14 +89,36 @@ class RasterFile:
 
         # band = self.dataset.GetRasterBand(2000)
 
+def create(filename, format='GTiff', origin=(0,0), pixel_size=(1.0, 1.0),
+    extent_in_pixels=(1,1), band_count=1, data_type=RasterDataType.BYTE, nodata_value=None) -> RasterFile:
+
+    origin_lng, origin_lat = origin
+    pixel_size_lng, pixel_size_lat = pixel_size
+    extent_in_pixels_lng, extent_in_pixels_lat = extent_in_pixels
+
+    driver = gdal.GetDriverByName(format)
+    dataset = driver.Create(filename, extent_in_pixels_lng, extent_in_pixels_lat, band_count, data_type.value)
+    dataset.SetGeoTransform([origin_lng, pixel_size_lng, 0, origin_lat, 0, -pixel_size_lat])
+    srs = osr.SpatialReference()
+    srs.SetUTM(11, 1)
+    srs.SetWellKnownGeogCS('NAD27')
+    dataset.SetProjection(srs.ExportToWkt())
+    return RasterFile(dataset=dataset)
+
 if __name__ == '__main__':
     
-    parser = optparse.OptionParser()
-    (options, args) = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('command', help='operation to perform')
+    parser.add_argument('filename', help='file to operate on')
+    args = parser.parse_args()
 
-    if len(args) != 1:
-        print("\n***** ERROR: Required argument path_to_raster_file was not provided *****\n")
-        exit()
-
-    dataset = RasterFile(args[0])
-    dataset.print_properties()
+    if args.command == 'create':
+        create(
+            args.filename,
+            pixel_size=(0.0083333,0.0083333),
+            extent_in_pixels=(12,12),
+            origin=(-114.0, 43.0)
+        )
+    elif args.command == 'probe':
+        dataset = RasterFile(args.filename)
+        dataset.print_properties()
--- packages/skope/test_raster_file_probe.py
+++ packages/skope/test_raster_file_probe.py
@@ -7,8 +7,8 @@ name_of_existing_file = 'test-1x1x1-byte.tif'
 name_of_nonexisting_file = 'no_such_file'
 
 @pytest.fixture
-def properties():
-    return RasterFile(name_of_existing_file).properties
+def dataset():
+    return RasterFile(name_of_existing_file)
 
 def test_init_with_existing_file_succeeds():
     dataset = RasterFile(name_of_existing_file)
@@ -17,8 +17,8 @@ def test_init_with_nonexisting_file_raises_exception():
     with pytest.raises(Exception):
         dataset = RasterFile(name_of_nonexisting_file)
 
-def test_get_dataset_properties_filename(properties):
-    assert properties['filename'] == name_of_existing_file
+def test_get_dataset_filename(dataset):
+    assert dataset.filename == name_of_existing_file
 
 if __name__ == '__main__':
     pytest.main()
